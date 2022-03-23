2a9908037715112b68a3c75db7a2b73de73c91f5
“GamerCleanVic” <jonassd2000@gmail.com>
2022-03-23 15:50:44 -0500

    +Tabuada do 2 PHP

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
-print_r("mudei o texto aq");
+//TABUADA
+$a = 2;
+echo ("TABUADA DO $a<br><br>");
+echo ("SOMA<br>");
+for($i=1;$i<=10;$i++){
+echo("$a + $i = ".$a + $i."<br>");
+}
+echo("<br><br>");
+
+echo ("SUBTRAÇÃO<br>");
+for($i=1;$i<=10;$i++){
+echo("$a - $i = ".$a - $i."<br>");
+}
+echo("<br><br>");
+
+echo ("MULTIPLICAÇÃO<br>");
+for($i=1;$i<=10;$i++){
+echo("$a x $i = ".$a * $i."<br>");
+}
+echo("<br><br>");
+
+echo ("DIVISÃO<br>");
+for($i=1;$i<=10;$i++){
+echo("$a / $i = ".$a / $i."<br>");
+}
+echo("<br><br>");
+
+
 ?>
