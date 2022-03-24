e91fc330a95ecb1ce9adce2caac8cde769686111
“GamerCleanVic” <jonassd2000@gmail.com>
2022-03-23 21:26:40 -0500

    +gitignore, +tabuada

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
+<!DOCTYPE html>
+<html>
+<html>
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="css/tab.css">
+<title>TABUADA WEB</title>
+</head>
+<body>
+<h1>DIGITE UM NÚMERO INTEIRO</h1>
+<!-- RISCO DE INJECT (NÃO SEI FAZER AINDA CONDIÇÕES PARA != DE NºS OU PARA CAMPO EM BRANCO) -->
+<form method="post" action="" class="form1">
+<input type="text" id="a" name="a" class="caixa1">
+<input type="submit" value="CONFIRMAR" class="btn1">
+</form><br>
+</body>
+</html>
 <?php
+$a = 10;
+if(isset($_POST['a'])){
+    $a = $_POST['a'];
+}
+
 //TABUADA
-$a = 2;
 echo ("TABUADA DO $a<br><br>");
 echo ("SOMA<br>");
 for($i=1;$i<=10;$i++){
@@ -25,6 +46,5 @@ for($i=1;$i<=10;$i++){
 echo("$a / $i = ".$a / $i."<br>");
 }
 echo("<br><br>");
-
-
 ?>
+
